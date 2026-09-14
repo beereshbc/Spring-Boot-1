d99b702ce908a5bfde02fd828c3d4c7f4446f3dd
beereshbc <bcbeereshkumar@gmail.com>
2026-09-14 12:32:34 +0530

    POST and DELETE Restful APIs

--- src/main/java/com/springboot/javaproject1/Student/StudentRepository.java
+++ src/main/java/com/springboot/javaproject1/Student/StudentRepository.java
@@ -1,8 +1,13 @@
 package com.springboot.javaproject1.Student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface StudentRepository extends
         JpaRepository<Student, Long> {
 
+    @Query("SELECT s FROM Student s WHERE s.email =?1")
+    Optional<Student> findStudentByEmail(String email);
 }
--- src/main/java/com/springboot/javaproject1/Student/StudentService.java
+++ src/main/java/com/springboot/javaproject1/Student/StudentService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Component //Component gives the priority to run the classes in java
 public class StudentService {
@@ -18,11 +19,22 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
-        System.out.println("Controller reached");
-        System.out.println(student);
+        Optional<Student> studentByEmail = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentByEmail.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
     }
 
     public List<Student> getStudent(){
          return studentRepository.findAll();
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists){
+            throw new IllegalStateException("student with id " + studentId + " does not exist");
+        }
+        studentRepository.deleteById(studentId);
     }
 }
